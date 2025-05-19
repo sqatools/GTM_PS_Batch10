82d5715283e79d8a77b29cbf08fc6ed070c26821
Nagaratna940 <nagaratnanaik940@gmail.com>
2025-05-19 20:24:01 +0530

    My First file

---
+++ Nagaratna/Myprogramming.py
@@ -0,0 +1,18 @@
+a=10
+print(a)
+b=20
+print(a+b)
+print("-"*50)
+s=20
+t=30
+print("addition",s+t)
+Sub=s+t
+print("Addition values is",Sub)
+print("-"*50)
+d1= 75.87
+d2=3
+print("single vslues:",d1/d2)
+print("double values:",d1//d2)
+print("-"*50)
+q=10
+print("square of 10",**10)
