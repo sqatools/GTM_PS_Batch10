406d04de47e9ca55ff2b34c911d203bd033039ac
knisha2511 <khandagalenisha@gmail.com>
2025-05-19 23:54:44 +0530

    Simple python program practice

---
+++ NishaK/PythonProgramming/Variables/Python_Variables.py
@@ -0,0 +1,93 @@
+# 1. Python Program to add two integer values.
+a = 50
+b = 30
+print("Addition of a & b :", a+b)
+print(id(a))
+
+
+print("-"*50)
+# 2. Python Program to subtract two integer values.
+V1 = 95
+V2 = 45
+print("Subtraction of V1 & V2 :", V1-V2)
+
+
+print("-"*50)
+# 3. Python program to multiply two numbers.
+A1 = 10
+B1 = 40
+print("Multiplication of A1 & B1 :", A1*B1)
+
+
+print("-"*50)
+# 4. Python program to repeat a given string 5 times.
+String1 = "Nisha"
+n1 = 5
+print("Output :", String1*n1)
+
+
+print("-"*50)
+# 5. Python program to get the Average of given numbers.
+N1 = 40
+N2 = 50
+N3 = 60
+N4 = 70
+print("Average of N1 N2 N3 & N4 :", N1+N2+N3+N4/4)
+
+
+print("-"*50)
+# 6. Python program to print the square and cube of a given number.
+C1 = 10
+print("Square of C1 :", C1**2)
+print("Cube of C1 :", C1**3)
+
+
+print("-"*50)
+# 7. Python program to interchange values between variables.
+H1 = 25
+H2 = 45
+H1, H2 = H2, H1
+print("Value of H1 :", H1)
+print("Value of H2 :", H2)
+
+print("-"*50)
+# 8. Python program to solve this Pythagoras theorem.
+#Theorem : a^2+b^2=c^2
+#Result : a**2+b**2
+a = 2
+b = 3
+print("Hypotenuse C^2 :", a**2+b**2)
+
+
+print("-"*50)
+# 9. Python program to solve the given math formula.
+# Formula : (a + b)2 = a^2 + b^2 + 2ab
+# Result : a**2 + b**2 + 2*a*b
+a = 2
+b = 3
+print("Value of (a + b)2 :", a**2 + b**2 + 2*a*b)
+
+
+print("-"*50)
+# 10. Python program to solve the given math formula.
+# Formula : (a – b)2 = a^2 + b^2 – 2ab
+# Result : a**2 + b**2 - 2*a*b
+a = 4
+b = 5
+print("Value of (a – b)2 :", a**2 + b**2 - 2*a*b)
+
+
+print("-"*50)
+# 11. Python program to solve the given math formula.
+# Formula : a^2 – b^2 = (a-b)(a+b)
+# Result : (a-b)*(a+b)
+a = 7
+b = 6
+print("Value of a^2 – b^2 :", (a-b)*(a+b))
+
+
+print("-"*50)
+# 12. Python program to calculate the area of the square.
+# Formula : area = a*a
+a = 4
+print("Area of square :", a*a)
