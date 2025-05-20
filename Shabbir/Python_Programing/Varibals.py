9411518d92ee14b0b2f446e62100c959e8c9dfbc
Shabbir_G <shabbirg470@gmail.com>
2025-05-20 19:46:27 +0530

    variables

---
+++ Shabbir/Python_Programing/Varibals.py
@@ -0,0 +1,25 @@
+
+# p = 50
+# r = 20
+# t = 2
+#
+# Total = (p* r* t)/100
+# print(Total)
+
+# wap calucated the compound interst
+
+# p = 500
+# r = 10
+# n = 5
+# t = 3
+#
+# ci = p*(1+r/n)^nt-p
+# print(ci)
+
+# Wap find the area of shere
+
+# formal = 4*pi*r*2
+
+r = 10
+area = 4*3.14*r*r
+print(area)
