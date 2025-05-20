76e6b2814de8bec554928f102e917ac14eeb1598
Shabbir_G <shabbirg470@gmail.com>
2025-05-20 19:52:10 +0530

    variables

--- Shabbir/Python_Programing/Varibals.py
+++ Shabbir/Python_Programing/Varibals.py
@@ -8,18 +8,18 @@
 
 # wap calucated the compound interst
 
-# p = 500
-# r = 10
-# n = 5
-# t = 3
-#
-# ci = p*(1+r/n)^nt-p
-# print(ci)
+p = 500
+r = 10
+n = 2
+t = 3
+
+CI = p*(1+r/n)**(n*t)-p
+print(CI)
 
 # Wap find the area of shere
 
 # formal = 4*pi*r*2
 
-r = 10
-area = 4*3.14*r*r
-print(area)
+# r = 10
+# area = 4*3.14*r*r
+# print(area)
