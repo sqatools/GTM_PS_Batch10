442cf1c0ef1eeafafafe79418566ccf56da5504d
swaroop <swaroop.reddi@yahoo.com>
2025-05-20 22:17:26 +0530

    Practice of HW

---
+++ Swaroop/Python Programming/Variables/python_var_practice.py
@@ -0,0 +1,57 @@
+#simple intrest
+"""
+p = 100000
+r = 2.5
+t = 1
+s = ("simple intrest :", p * r * t/100)
+print(s)
+"""
+
+#compount intrest
+""""
+p = 10000
+r = 5
+t = 1
+n = 2
+c = p * (1+r/n) ** (n*t)-p
+print(c)
+"""
+
+#Find area of sphere
+pi = 3.21
+r = 2
+f = 4 * pi * r **2
+print(f)
+
+#Addin two numbers
+a = 10
+b = 20
+print("Adding two numbers:", a+b)
+print("address of a:", id(a))
+print("address of b:", id(b))
+
+#Susbtract two nubers
+c = 1
+d = 2
+print("Substract values :", d-c)
+
+#multiply two numbers
+e = 4
+f = 5
+print("Multiply two values:", 4*5)
+
+#Repeat a string 5 times
+str1 = "hello"
+g = 5
+print("multiple times string:", str1 * 5)
+
+#Average of given numbers
+h = 10
+i = 20
+j = 30
+print("average of all the numbers :", h+i+j/3)
+
+
+
+
+
