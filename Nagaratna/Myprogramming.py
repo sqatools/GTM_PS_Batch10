9b593855920cf3181393ecd4366a644c323ef196
Nagaratna940 <nagaratnanaik940@gmail.com>
2025-05-21 19:31:39 +0530

    add

--- Nagaratna/Myprogramming.py
+++ Nagaratna/Myprogramming.py
@@ -1,18 +1,49 @@
-a=10
-print(a)
-b=20
-print(a+b)
-print("-"*50)
-s=20
-t=30
-print("addition",s+t)
-Sub=s+t
-print("Addition values is",Sub)
-print("-"*50)
-d1= 75.87
-d2=3
-print("single vslues:",d1/d2)
-print("double values:",d1//d2)
-print("-"*50)
-q=10
-print("square of 10",**10)
+P= 100000
+R=10.5
+T=5
+si=(P*R*T)/100
+print("Simple interst is :",si)
+#Area of square:
+PI=3.21
+R=3
+formula=4*PI*R**2
+print("area of square:",formula )
+# compound interest:
+P=60000
+r=6
+n=4
+t=2
+CI=P*(1+r/n)**(n*t)-P
+print("Compound interest :", CI)
+
+
+#Adding two integer
+nu1=40
+nu2=80
+print("Adding two number total is",nu1+nu2)
+
+#substract two integer value:
+m=1300
+m1=10
+print("substraction of value is",m/m1)
+
+#multiply two number
+mul=7
+mul1=18
+print("multiplication of number is",mul*mul1)
+
+print("____________multiply given string")
+str="SQTOTOL"
+n1=5
+print("multiply string is:",str*5)
+
+print("____________Avarage of number__________")
+a=60
+b=30
+c=40
+d=120
+avg=(a+b+c+d)/4
+print("Avg number is",avg)
+
+
+
