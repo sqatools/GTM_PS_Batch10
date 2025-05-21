63dea179188c01184b6500471a2af8e69f559914
sruthi273030 <sruthikd91@gmail.com>
2025-05-21 15:37:01 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ sruthi/variables.py
@@ -0,0 +1,55 @@
+# 1. Python Program to add two integer values.
+import math
+
+a = 50
+b = 30
+print("# 1. Addition of a & b :", a+b)
+
+c = a+b
+print("Value of c :", c)
+print(id(a))
+
+# 2. Python Program to subtract two integer values.
+V1 = 95
+V2 = 45
+V3 = V1-V2
+print("# 2. Subtraction of V1 & V2 :", V1-V2)
+print("Value of V3 :", V3)
+
+# 3. Python program to multiply two numbers.
+A1 = 60+30
+B1 = 40
+C1 = A1*B1
+print("# 3. Multiplication of A1 & B1 :", A1*B1)
+print("Value of C1 :", C1)
+
+# 4. Python program to repeat a given string 5 times.
+String = "sruthi"
+n = 8
+print("# 8. Output :", String*n)
+
+# 5. Python program to get the Average of given numbers.
+u1 = 20
+u2 = 70
+u3 = 60
+u4 = 70
+print("# 5. Average of u1 u2 u3 & u4 :", u1+u2+u3+u4/4)
+
+# 11. Python program to solve the given math formula.
+# Formula : (a–b)^2 = a^2 + b^2 – 2ab
+# Result : a**2 + b**2 - 2*a*b
+a = 4
+b = 9
+print("# 11. Value of (a–b)^2 :", a**2 + b**2 - 2*a*b)
+
+#13. Python program to solve the given math formula.
+# Formula : (a+b)^3 = a^3 + 3ab(a+b) + b^3
+# Result : a**3 + 3*a*b*(a+b) + b**3
+a = 3
+b = 9
+print("# 13. Value of (a+b)^3 :", a**3 + 3*a*b*(a+b) + b**3)
+
+
+
+
+
