e096d43b434f91596349d22f383b66d90ee8d454
Chanakyakarnati <chanakyakarnati@gmail.com>
2025-05-25 08:36:01 +0530

    Added updated variable file

---
+++ Chanakya/Chanakya_Directory/variable/Pyton Variable_Learning.py
@@ -0,0 +1,77 @@
+
+#######################
+a = 10
+print("a values is ", a)
+
+print("memory of the a value iis " , id(a))
+b = 20
+c = 20
+
+print("memory of the a b c vlaues are " , id(a), id(b), id(c))
+
+x = y = z = 100
+
+z = 20
+print("values of a, b, c, x, y, z" , a, b, c ,x,y,z,(id(a)), ("*"*5)) # storing the values
+
+
+print("*"*100)
+#################
+a =10;
+print("first a value is the : " , a)
+
+
+a =100;
+print("second a value is the : " , a)
+
+a=1000
+print("Third time a value is the : " , a)
+
+
+Fourth_Time_A_Value_is = 10000
+print("fourth time a value is the " , Fourth_Time_A_Value_is)
+
+var122 = 100;
+
+Name ="Chanakya"
+name ="chanikya"
+NamE="Karnati"
+NAME='ChanakyaKarnati'
+print("Name is the :",Name)
+print("name is the ", name)
+print("Name is the ", Name)
+print("NamE is the " , Name)
+
+print("All different type of names are " , Name , name, NamE, NAME)
+
+
+"""""
+
+Mutli line comment
+
++ : add
+- : substration
+* : Multipication
+== : Equals to
+!= : Not equals to
+/ = division with single
+// = division with dobule
+** = power operation
+
+
+"""""
+c = 10; d = 20
+print("adding the values are :" , c+d)
+print("substraction the values are : ", c-d)
+print("Mulitipication of values are : " , c*d)
+print("divisoin of the valus are : ", c/d)
+print("power operation of the values are " , c**d)
+print("double division fo the vlaues are : " , c//d)
+
+e = c+d
+print("print e value is the : " , e)
+
+print("adding the values are " , 500+e, c+d-e)
+
+d1=90; d5=980
+print("division of d1 and d5" , d1/d5)
