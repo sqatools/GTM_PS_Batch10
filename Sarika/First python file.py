c3c12b174240566b4ed07fa0d57094d34bc85f7a
saara456 <saars123444@gmail.com>
2025-05-26 14:22:58 +0530

    adding to the file

--- Sarika/First python file.py
+++ Sarika/First python file.py
@@ -41,5 +41,49 @@ print("_"*50)
 #56var = 789456 incorrect
 
 #3. There is no limit for variable name
-we_are_learning_python_programming = "Paython Programming" correct
+#we_are_learning_python_programming = "Python Programming" correct
 # m = 200 correct
+
+#4. Variable names are case sensitive
+name = 'Rohan'
+Name = 'Prateik'
+NAME = 'Sohan'
+NAMe = 'Shyam'
+print(name, Name, NAME, NAMe)
+
+print("_"*90)
+
+#5. The variable cannot contain special characters except underscore.
+#var$abc = "Python" Incorrect
+#var_abc_hello = "Correct"
+
+print("_"*80)
+#+ = addition
+# - = subtraction
+#/ = division with single/
+#// = division with double //
+#== = equal to
+#!= =not equal to
+#% = remainder operator
+#** = power operator
+
+#addition
+a1 = 40
+a2 = 50
+print("addition:", a1+a2)
+
+#Subtraction
+print("subtraction:", a1-a2)
+
+#Multiplication of values
+print("multiplication:", a1*a2)
+
+p = 50
+q = 60
+r = 40
+print("p-q-r:", p-q-r)
+
+#division of values
+a1 = 32
+a2 = 4
+print("a1/a2:", a1/a2)
