f5e84301a5bc2302c62e612becd8e382062a5d0d
Shabbir_G <shabbirg470@gmail.com>
2025-05-29 20:13:17 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/GTM_PS_Batch10.iml
    #	.idea/misc.xml

---
+++ Shabbir/Python_Programing/if_conditions/WFO.py
@@ -0,0 +1,6 @@
+# Q1. write a program to check the person is eligible to vote on the basis age > 18
+# Q2. write a program to calculate the electricity bill amount on the basic of unit consumtion.
+# 1. unit < 100 : per unit charge is 15
+# 2. unit > 100  and num < 200 : per unit charge is 200
+# 3. unit > 200 : per unit charge is 25
+
