29935968129f965b2ae5b9df53663a03266169ef
Shabbir_G <shabbirg470@gmail.com>
2025-05-29 20:43:36 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/GTM_PS_Batch10.iml
    #	.idea/misc.xml

--- Shabbir/Python_Programing/if_conditions/WFO.py
+++ Shabbir/Python_Programing/if_conditions/WFO.py
@@ -4,3 +4,24 @@
 # 2. unit > 100  and num < 200 : per unit charge is 200
 # 3. unit > 200 : per unit charge is 25
 
+
+# Q1. write a program to check the person is eligible to vote on the basis age > 18
+
+
+"""
+age = int(input("Please enter Candidate the age for Vote :"))
+if age >=18:
+    print("The Candidate is eligible for Vote")
+else:
+    print("The Candidate is not Eligible for Vote")
+"""
+
+unit1 = float(input("Enter the electricity bill amount : "))
+if unit1 < 100:
+    charge = unit1 * 15
+elif unit1 <= 200:
+    charge = unit1 * 20
+else: charge = unit1 * 25
+print("The Total Bill amount is :",charge)
+
+
