35bbaea4044f22faf000d4b81aecb1cacf565c01
ArchanaMani06 <archanask206@gmail.com>
2025-05-30 22:08:24 +0900

    Added todays Home work

---
+++ Archana/Python_Programming/Homework/DataType/list_dataType.py
@@ -0,0 +1,8 @@
+# 1.Program to find the Largest Number in a List
+l1 = [23, 45, 34, 46, 56]
+print("Largest number is:", max(l1))
+
+# 2.Program to find the second-largest number in a List
+l1 = [23, 45, 34, 46, 56]
+print("Largest number is:", max(l1))
+
---
+++ Archana/Python_Programming/if_condition.py
@@ -0,0 +1,69 @@
+# 1. Program to compare two numbers
+a = 10
+b = 20
+if a == b:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
+
+print("_"*50)
+
+# 2. Program to check the given value is even or odd
+
+num1 = int(input("Enter any number:"))
+if num1 % 2 == 0:
+    print("The number is even number")
+else:
+    print("The number ais odd number")
+
+""" 
+# AND condition
+# Logical operators
+> : Greater than
+< : Less than
+>= : Greater thn equal to
+<= : Less than equal to
+==: Equal to
+!=: Not Equal to
+
+in : in operator
+not in : not in operator
+is : is operator
+is not : is not operator
+
+"""
+
+# 3. Program  to check if a given number is divisible by
+
+
+
+
+#4. Program to find the result of interview using nested if condition
+round1 = "pass"
+round2 = "fail"
+round3 = "pass"
+if round1 == "pass":
+    print("First round is cleared")
+    if round2 == "pass":
+        print("Second round is cleared")
+        if round3 == "pass":
+            print("Third round is cleared")
+        else:
+            print("Third round is not cleared")
+    else:
+        print("Second round is not cleared")
+else:
+    print("First round is not cleared")
+
+# Homework
+# 1. Write a program to check a person is eligible to vote
+# 2. Write a program to calculate the EB bill amount on the basis of the unit consumptions
+if unit > 100 :
+    charge is 15
+if unit < 100 and unit < 200:
+    charge is 15
+    
+
+
+
+
