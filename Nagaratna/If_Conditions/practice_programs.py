4497cabf69049ecc070e779b732468f6fd18affc
Nagaratna940 <nagaratnanaik940@gmail.com>
2025-06-02 19:00:24 +0530

    Adding new file 2

---
+++ Nagaratna/If_Conditions/practice_programs.py
@@ -0,0 +1,14 @@
+#########Python program to check given number is divided by 3 or not.
+z= 45
+if z%3 ==0:
+     print(" a is dividable",z)
+else:
+     print("a is not dividable", z)
+print("__-**-__"*5)
+##Python program to check number is divided by 3 and 5##
+
+g = 75
+if g%3 == 0 and g%5 == 0:
+     print("its divisible" , g)
+else:
+     print("its not divicible" , g)
---
+++ Nagaratna/Variable/reverse_string.py
@@ -0,0 +1,3 @@
+num = int(5678)
+reverse = str(num)
+print("Reverse: ",reverse[::-1])
