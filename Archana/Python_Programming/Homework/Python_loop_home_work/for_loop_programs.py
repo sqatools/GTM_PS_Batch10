3c850dce477f036108ee915a2459de335ce0d4d1
ArchanaMani06 <archanask206@gmail.com>
2025-06-03 18:25:06 +0900

    added the nested for loop programs

--- Archana/Python_Programming/Homework/Python_loop_home_work/for_loop_programs.py
+++ Archana/Python_Programming/Homework/Python_loop_home_work/for_loop_programs.py
@@ -17,7 +17,6 @@ for i in range(1, 7):
                 print(" ", end=" ")
     print()
 
-
 """
 # write a program to print swastik pattern
 
@@ -58,3 +57,32 @@ for i in range(1, 9):
                 print(" ", end=" ")
     print()
 
+# write a program to print letter A  pattern
+print("-" * 50)
+for i in range(1, 9):
+    for j in range(1, 8):
+        if i in [1]:
+            if j in [4]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        elif i in [2]:
+            if j in [3, 5]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        elif i in [3]:
+            if j in [2, 6]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        elif i in [4]:
+            print("*", end=" ")
+        elif i in [5]:
+            print("*", end=" ")
+        elif i in [6, 7, 8, 9]:
+            if j in [1, 2, 6, 7]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+    print()
