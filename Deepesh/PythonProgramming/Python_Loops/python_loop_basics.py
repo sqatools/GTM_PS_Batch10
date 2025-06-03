c530e7773656632f0441b1cc20de8ea13c370871
Bi12Python <xovek11875@bitvoo.com>
2025-06-03 20:02:22 +0530

    for loops session3

--- Deepesh/PythonProgramming/Python_Loops/python_loop_basics.py
+++ Deepesh/PythonProgramming/Python_Loops/python_loop_basics.py
@@ -255,3 +255,187 @@ else:
     print("This is not an prime number :", num)
 
 
+print("_"*50)
+###########################################
+# write a program to get list of all the prime numbers from 1 to 100
+
+for num in range(1, 100):
+    prime = True
+    for i in range(2, num//2+1):
+        if num%i == 0:
+            prime = False
+            break
+        else:
+            continue
+
+    if prime:
+        print(num, end=" ")
+
+
+# 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+
+
+print()
+print("_"*50)
+############################ continue and break statement ###############
+#
+# continue statement :  If for value in matching with continue statement condition
+#                       then it will skip remaining code execution and move in iteration of foor loop
+for i in range(1, 10):
+    if i == 5 or i == 6:
+        continue
+
+    print(i)
+
+
+print("_"*50)
+# break statement :  If for loop value is matching with break condition then it will break the loop execution
+#                    and won't execute further code.
+
+for j in range(1, 10):
+    if j == 5:
+        break
+    print(j)
+
+print("_"*50)
+########################################
+# while loop in python:
+# while loop code execute until the certain condition is matching.
+
+n = 1
+
+while n <= 10:
+    print(n)
+    n += 1
+
+
+print("_"*50)
+# infinite loop
+"""
+n = 1
+
+while True:
+    print(n)
+    n += 1
+    if n == 100000:
+        break
+"""
+
+
+print("_"*50)
+# write a program to reverse any given number
+
+num = 782356
+# num = 123
+# output = 321
+reverse = 0
+
+while num > 0: # 12 > 0, 1 > 0, 0> 0
+    temp = num%10  # 3, 2, 1
+    reverse = reverse*10 + temp # 3  | 3*10 + 2 = 32 | 32*10 + 1 = 321
+    num = num//10 #  12, 1, 0
+
+
+print("reverse value : ", reverse)
+
+print("_"*50)
+#####################################  ASCII VALUES ################
+
+# Capital A-Z : 65-90
+# Small a-z : 97-122
+
+# ord() function :  ord function return the ascii value of given character
+
+
+# get ascii with character
+char = 'A'
+print(ord(char)) # 65
+####################################################################
+# chr() function :  chr function return the character of given ascii value.
+
+# get character name with ascii
+print(chr(90)) # Z
+
+for i in range(65, 91):
+    print(chr(i), end=" ")
+#
+#A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+print()
+for i in range(97, 123):
+    print(chr(i), end=" ")
+# a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+
+
+print()
+for i in range(1, 65):
+    print(chr(i), end=" ")
+
+# ! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @
+
+
+print()
+print("_"*50)
+###############################################
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for char in letters:
+    print(ord(char), end=" ")
+# 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90
+
+
+print()
+print("_"*50)
+##################################
+# write a program to print a given pattern with letter
+
+"""
+A
+B C
+D E F
+G H I J
+K L M N O
+"""
+
+num = 65
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(chr(num), end=" ")
+        num += 1
+
+    print()
+
+# ASCII: American Standard Code for Information Interchange
+
+
+print()
+print("_"*50)
+####################################################
+
+#  program to print below pattern
+"""
+A
+B C
+D E F
+G H I J
+K L M N O
+P Q R S
+T U V
+W X
+Y
+"""
+
+num = 65
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(chr(num), end=" ")
+        num += 1
+
+    print()
+
+for k in range(1, 5):
+    for l in range(5, 0+k, -1):
+        print(chr(num), end=" ")
+        num += 1
+
+    print()
