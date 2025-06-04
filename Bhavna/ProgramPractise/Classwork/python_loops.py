f5a36166d1eae6679e972244b1fb2758a1f91532
bhavnaprakash88 <bhavanaprakash888@gmail.com>
2025-06-04 16:44:04 +0530

    Python Loops

--- Bhavna/ProgramPractise/Classwork/python_loops.py
+++ Bhavna/ProgramPractise/Classwork/python_loops.py
@@ -154,6 +154,8 @@ for i in range(1,7):
 
     print()
 
+####### 03/06/2025 Session ###########
+
 # write a program to find out the given number is prime or not
 
 k = 12
@@ -171,3 +173,161 @@ else:
 
 
 print('_'*70)
+###########################################
+# write a program to get list of all the prime numbers from 1 to 100
+
+for k in range(1,100):
+    prime = True
+    for i in range(2, k//2+1):
+        if k%i == 0:
+            prime = False
+            break
+        else:
+            continue
+    if prime:
+        print(k, end=" ")
+
+print()
+print('_'*70)
+############################ continue and break statement ###############
+# continue statement: If for value is matching with continue statement condition
+# then it will skip remaining code & move to next for loop iteration
+
+for i in range(1,10):
+    if i == 3 or i == 9:
+        continue
+
+    print(i)
+
+print('_'*70)
+# Break Statement: if for loop value is matching with break condition
+# then it will break the loop execution & won't execute code further.
+
+for j in range(1,10):
+    if j == 6:
+        break
+    print(j)
+
+print('_'*70)
+
+########################################
+# while loop in python:
+# while loop code execute until the certain condition is matching.
+
+n = 1
+
+while n <= 10:
+    print(n)
+    n +=1
+
+print('_'*70)
+
+# infinite loop
+
+m = 1
+while True:
+    print(n)
+    n +=1
+    if n == 1000:
+        break
+
+print('_'*70)
+# write a program to reverse any given number
+
+num = 125
+reverse = 0
+
+while num > 0:
+    temp = num%10
+    reverse = reverse*10 + temp
+    num = num//10
+
+print("reverse value:", reverse)
+print('_'*70)
+
+#####################################  ASCII VALUES ################
+
+# Capital A-Z : 65-90
+# Small a-z : 97-122
+
+# ord() function :  ord function return the ascii value of given character
+# To get ascii value with character
+
+char = 'B'
+print(ord(char))
+
+####################################################################
+# chr() function :  chr function return the character of given ascii value.
+#To get character name by using ascii value
+
+l = 70
+print(chr(l))
+
+print(chr(75))
+
+for i in range(65,91):
+    print(chr(i), end=" ")
+
+print()
+
+for j in range(97,123):
+    print(chr(j), end=" ")
+
+print()
+print('_'*70)
+
+###############################################
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for char in letters:
+    print(ord(char), end=" ")
+
+print()
+print('_'*70)
+
+##################################
+# write a program to print a given pattern with letter
+
+"""
+A
+B C
+D E F
+G H I J
+K L M N O
+"""
+
+num = 65
+for i in range(1,6):
+    for j in range(1, i+1):
+        print(chr(num), end=" ")
+        num += 1
+
+    print()
+
+print("_"*70)
+####################################################
+
+#  program to print below pattern
+"""
+A
+B C
+D E F
+G H I J
+K L M N O
+P Q R S
+T U V
+W X
+Y
+"""
+
+num = 65
+for i in range(1,6):
+    for j in range(1, i+1):
+        print(chr(num), end=" ")
+        num +=1
+    print()
+for k in range(1,5):
+    for l in range(5,k,-1):
+        print(chr(num),end=" ")
+        num +=1
+    print()
+
