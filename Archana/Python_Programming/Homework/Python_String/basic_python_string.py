20f1869c58bc602eb4a11a47a2a517047bda42d0
ArchanaMani06 <archanask206@gmail.com>
2025-06-05 22:21:45 +0900

    Added changes to string programs

--- Archana/Python_Programming/Homework/Python_String/basic_python_string.py
+++ Archana/Python_Programming/Homework/Python_String/basic_python_string.py
@@ -1,80 +1,80 @@
-# """
-# # Home Work
-# # str1 = "Virat is greate Indian Player"
-# output1 = "Player is greate Indian Virat"
-# output2 = "VVirat is greate Indian Playerr"
-# output3 = "VViratt iiss ggreatee IIndiann PPlayerr"
-# output4 = "(Virat is greate): reverse this part : Indian Player"
-# """
-# print("#Output 1")
-# print("*" * 10)
-# str1 = "Virat is great Indian Player"
-# # output1 = "Player is great Indian Virat"words
-# words = str1.split()  # ['Virat', 'is', 'great', 'Indian', 'Player']
-# words[0], words[-1] = words[-1], words[0]
-# output1 = ' '.join(words)
-# print(output1)
-#
-# print("-" * 50)
-# print("#Output 2")
-# print("*" * 10)
-# words = str1.split()  # ['Virat', 'is', 'great', 'Indian', 'Player']
-# words[0] = 'V' + words[0]
-# words[4] = words[4] + 'r'
-# output2 = ' '.join(words)
-# print(output2)
-#
-# print("-" * 50)
-# print("#Output 3")
-# print("*" * 10)
-# # output3 = "VViratt iiss ggreat IIndiann PPlayerr"
-# str1 = "Virat is great Indian Player"
-# # rule: add first letter + word + last letter
-# words = str1.split()
-# modified_words = []
-# for char in words:
-#     modified_word = char[0].upper() + char + char[-1].lower()
-#     modified_words.append(modified_word)
-#
-# output3 = ' '.join(modified_words)
-# print(output3)
-#
-# # .1). Write a Python program to get a string made of the
-# # first and the last 2 chars from a given string.
-# # If the string length is less than 2,
-# # return instead of the empty string
-# str1 = input("Enter the String:")
-# if len(str1) > 2:
-#     new_str = str1[:2] + str1[-2:]
-#     print(new_str)
-# else:
-#     print("Cannot make a new word", end=" ")
-#
-# # 2). Python string program that takes a list of strings and returns the length of the longest string.
-#
-# str_list1 = ["Apple", "Banana", "Jack Fruit", "Strawberry", "Raspberry"]
-# temp_len = 0
-# for word in str_list1:
-#     if len(word) > temp_len:
-#         temp_len = len(word)
-#         longest = word
-# print("The Length of longest string is:", temp_len)
-# print("The longest string is:", longest)
-#
-# # 3). Python string program to reverse a string if it’s length is a multiple of 4.
-# str2 = input("Enter the String:")
-# len_str2 = len(str2)
-# if len_str2 % 4 == 0:
-#     print(str2[::-1])
-# else:
-#     print("Cannot reverse the word", end=" ")
-#
-# # 4).Python string program to test whether a passed letter is a vowel or consonant.
-#
-# vowels = "aeiou"
-# str3 = input("Enter the letter:").lower()
-# if str3 in vowels:
-#     print("The letter is a vowel:", str3)
+"""
+# Home Work
+# str1 = "Virat is greate Indian Player"
+output1 = "Player is greate Indian Virat"
+output2 = "VVirat is greate Indian Playerr"
+output3 = "VViratt iiss ggreatee IIndiann PPlayerr"
+output4 = "(Virat is greate): reverse this part : Indian Player"
+"""
+print("#Output 1")
+print("*" * 10)
+str1 = "Virat is great Indian Player"
+# output1 = "Player is great Indian Virat"words
+words = str1.split()  # ['Virat', 'is', 'great', 'Indian', 'Player']
+words[0], words[-1] = words[-1], words[0]
+output1 = ' '.join(words)
+print(output1)
+
+print("-" * 50)
+print("#Output 2")
+print("*" * 10)
+words = str1.split()  # ['Virat', 'is', 'great', 'Indian', 'Player']
+words[0] = 'V' + words[0]
+words[4] = words[4] + 'r'
+output2 = ' '.join(words)
+print(output2)
+
+print("-" * 50)
+print("#Output 3")
+print("*" * 10)
+# output3 = "VViratt iiss ggreat IIndiann PPlayerr"
+str1 = "Virat is great Indian Player"
+# rule: add first letter + word + last letter
+words = str1.split()
+modified_words = []
+for char in words:
+    modified_word = char[0].upper() + char + char[-1].lower()
+    modified_words.append(modified_word)
+
+output3 = ' '.join(modified_words)
+print(output3)
+
+# .1). Write a Python program to get a string made of the
+# first and the last 2 chars from a given string.
+# If the string length is less than 2,
+# return instead of the empty string
+str1 = input("Enter the String:")
+if len(str1) > 2:
+    new_str = str1[:2] + str1[-2:]
+    print(new_str)
+else:
+    print("Cannot make a new word", end=" ")
+
+# 2). Python string program that takes a list of strings and returns the length of the longest string.
+
+str_list1 = ["Apple", "Banana", "Jack Fruit", "Strawberry", "Raspberry"]
+temp_len = 0
+for word in str_list1:
+    if len(word) > temp_len:
+        temp_len = len(word)
+        longest = word
+print("The Length of longest string is:", temp_len)
+print("The longest string is:", longest)
+
+# 3). Python string program to reverse a string if it’s length is a multiple of 4.
+str2 = input("Enter the String:")
+len_str2 = len(str2)
+if len_str2 % 4 == 0:
+    print(str2[::-1])
+else:
+    print("Cannot reverse the word", end=" ")
+
+# 4).Python string program to test whether a passed letter is a vowel or consonant.
+
+vowels = "aeiou"
+str3 = input("Enter the letter:").lower()
+if str3 in vowels:
+    print("The letter is a vowel:", str3)
 
 # 5). Find the longest and smallest word in the input string.
 str4 = "This is sentence with long and short words"
@@ -93,6 +93,22 @@ for word2 in words_list:
         Short_word = word2
 print("The Shortest word is:", Short_word)
 
+# 6). Write a python program to replace the words “Java” with “Python” in the given
+# Input = “JAVA is the Best Programming Language in the Market”
+# Output = “Python is the Best Programming Language in the Market”
+in_str = "JAVA is the Best Programming Language in the Market"
+new_word = in_str.replace("JAVA", "Python")
+print(new_word)
 
+# # 7). Python string program to get a string made of 4 copies of the
+# # last two characters of a specified string (length must be at least 2).
+# str4 = input("Enter the string:")
+# print(str4[-2:]*4)
 
+# 8). Python string program to count occurrences of a substring in a string.
+str4 = "school"
+sub_string = "ol"
+#Printng output
+count =str4.count(sub_string)
+print(count)
 
