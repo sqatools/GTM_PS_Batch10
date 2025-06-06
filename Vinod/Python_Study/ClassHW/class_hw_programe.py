b13c45c9ff541cd0f7a63e3cd30a34b0e4593527
Vinod-24 <vinodneware24@gmail.com>
2025-06-06 23:45:19 +0530

    if condition program

---
+++ Vinod/Python_Study/ClassHW/class_hw_programe.py
@@ -0,0 +1,32 @@
+# hieght = 18
+# #int(input("what is your hieght in feet"))
+# if hieght >= 3 :
+#     print("yes can ride")
+#     age = 21
+#     if age <= 20 :
+#         print("pay 25 rs")
+#     else:
+#         print("rs 500")
+# else:
+#     print("can't ride")
+# print("bye")
+
+
+hieght = 18
+#int(input("what is your hieght in feet"))
+if hieght >= 3 :
+    print("yes can ride")
+    age = 13
+    if age < 12:
+        print("pay 25 rs")
+    elif age <=18:
+        print("pay 250 rs")
+    else:
+        print("rs 500")
+else:
+    print("can't ride")
+print("bye")
+
+
+
+
