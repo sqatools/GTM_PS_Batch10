72e9902ea49732259309a75c22fecd50b766b3dc
Nagaratna940 <nagaratnanaik940@gmail.com>
2025-06-08 01:15:51 +0530

    homework

--- Nagaratna/If_Conditions/Homework3.py
+++ Nagaratna/If_Conditions/Homework3.py
@@ -1,4 +1,11 @@
-    #####Program to get all numbers divided by 3 between 1 to 30
+# Python program to print the square of the number if it is divided by 11.
+print("*-_-" * 20)
+i = int(input("enter your value"))
+if i % 11 == 0:
+    print("square of the values is", i ** 2)
+else:
+    print("i is not divicible by 11", i)
+#####Program to get all numbers divided by 3 between 1 to 30
 for i in range(1,31):
         if i%3 ==0:
             print(i,end=" ")
@@ -25,5 +32,30 @@ elif marks > 90 and marks <= 100:
     print("Excellent")
 else:
    print("Invalid marks")
+
+#Program to check whether the number is a prime number.
+print("*-_-"*20)
+num = int(input("Enterrequired number"))
+count =0
+if i in range(2,num):
+    if num%i==0:
+        count+=1
+    if count>0:
+        print("entered number is a prime number")
+else:
+        print("its not a prime number")
+#################################
+j = int(input("Enter your j values"))
+count = 0
+if i in range(1,j):
+    count+=1
+    if i%j==1:
+        if count>1:
+            print("its a odd number")
+else:
+    print("its not prime number")
+
+
+
 
 
--- Nagaratna/If_Conditions/check_fibonace.py
+++ Nagaratna/If_Conditions/check_fibonace.py
@@ -1,6 +1,23 @@
 fibo = [23,5,90,64,12,45]
-numb =int(input("enter your gibo number"))
+numb =int(input("enter your fibo number"))
 if numb in fibo:
     print("number is is fibo",numb)
 else:
     print("number not in fibo", numb)
+########################
+### Program to check the authentication of username and password###################
+print("*-_-"*10)
+username=input("Enter Your Name")
+password= input("Enter your password")
+if username==password:
+    print("Its valid")
+else:
+    print("its not valid")
+########################################################
+print("*-_-"*10)
+F = (44,67,78,90,2,34.67)
+I = int(input("enter your number to check fibo"))
+if I in F:
+    print("Value avaibale under fibo",I)
+else:
+    print("Value not avaibale under fibo",I)
