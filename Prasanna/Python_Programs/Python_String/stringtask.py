9d5938d92af744af66f8664e9d593f546bc7fc59
PrasannaGit2025 <prasanna.kamana24@gmail.com>
2025-06-09 10:56:28 +0530

    string task completed

--- Prasanna/Python_Programs/Python_String/stringtask.py
+++ Prasanna/Python_Programs/Python_String/stringtask.py
@@ -1,12 +1,22 @@
-# Home Work
-"""
-# str1 = "Virat is great Indian Player"
-output1 = "Player is greate Indian Virat"
-output2 = "VVirat is greate Indian Playerr"
-output3 = "VViratt iiss ggreatee Indiann PPlayerr"
-output4 = "(Virat is greate): reverse this part : Indian Player"
-"""
-str1 = "Virat is greate Indian Player"
-words = sentence.split()
-rearranged = f"{words[4]} {words[1]} {words[2]} {words[3]} {words[0]}"
-print(rearranged)
+str1 = "Virat is great Indian Player"
+#0utput1 = "Player is great Indian Virat"
+w1 = str1[-7:]
+w2 = str1[5:-7]
+w3 = str1[:5]
+output1 = f"{w1} {w2} {w3}"
+print( "_"*50)
+print(output1)
+"""output2 = "VVirat is great Indian Playerr" """
+w4 = str1[:1]
+w5 = str1[0:]
+w6 = str1[27:]
+output2 = f"{w4}{w5}{w6}"
+print( "_"*50)
+print(output2)
+#output4 = "(Virat is great): reverse this part : Indian Player"
+w7 = str1[-13:]
+w8 = str1[0:14]
+output4 = f"{w7} {w8}"
+print( "_"*50)
+print(output4)
+
