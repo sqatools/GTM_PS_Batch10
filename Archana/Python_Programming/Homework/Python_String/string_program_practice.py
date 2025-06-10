dd805d0c1d9780f14db577c167885b7aae35985e
ArchanaMani06 <archanask206@gmail.com>
2025-06-10 11:17:34 +0900

    Adding string Home work programs

--- Archana/Python_Programming/Homework/Python_String/string_program_practice.py
+++ Archana/Python_Programming/Homework/Python_String/string_program_practice.py
@@ -31,7 +31,7 @@ for word in word_list:
         continue
 
 print("Output :", output)
-#Output : Rahul Manoj Nisha Satya
+# Output : Rahul Manoj Nisha Satya
 
 
 
@@ -57,12 +57,30 @@ print("Max len :", max_len) # Max len : 11
 print("Max len word :", max_len_word) # Max len word : Programming
 
 
-############ Home Work ############
-#Q1 write a python program to get palindrom word from given string.
+# ########### Home Work ############
+# Q1 write a python program to get palindrome word from given string.
 # palindrome :  it word and reverse of word is same then it is palindrome.
-str5 = "eWe Are AAAvAAE ThphT  OOPOO LLRLLE"
+str5 = "eWe Are AAAvAAE ThphT OOPOO LLRLLE"
 output = "eWe ThphT OOPOO"
+words_list = str5.split(" ")
+pal_word_count = 0
+for char in words_list:
+    if char[::-1] == char:
+        pal_word_count += 1
+        print("The  palindrome word is:", char)
+    else:
+        continue
+print("The number of palindrome words are:", pal_word_count)
 
-#Q2 write a python program count all the vowels from given string.
+
+# Q2). write a python program count all the vowels from given string.
 str6 = "Python Programming"
-#output = 4
+vowels_list = "aeiou"
+# output = 4
+count = 0
+for char in str6:
+    if char in vowels_list:
+        count += 1
+    else:
+        continue
+print("Number of vowels:", count)
