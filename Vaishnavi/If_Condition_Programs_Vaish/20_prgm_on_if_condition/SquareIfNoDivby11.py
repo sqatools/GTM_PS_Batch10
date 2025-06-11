7bbfa2cbbdff3af7902ee0e468561edd7be04247
Vaishnavi22111995 <vaishnavirajput111995@gmail.com>
2025-06-11 22:25:09 +0530

    Adding 1st python file

---
+++ Vaishnavi/If_Condition_Programs_Vaish/20_prgm_on_if_condition/SquareIfNoDivby11.py
@@ -0,0 +1,8 @@
+#Python program to print the square of the number if it is divided by 11
+
+Val=int(input("Enter the value"))
+
+if Val%11==0:
+    print(Val,"is div by 11"," ","its square no is :",Val**2)
+else:
+    pass
