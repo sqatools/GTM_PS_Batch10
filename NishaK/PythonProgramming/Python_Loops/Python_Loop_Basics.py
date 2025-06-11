e88386c0de9bf08806b00c7ad624b974b1c003e4
knisha2511 <khandagalenisha@gmail.com>
2025-06-11 13:36:51 +0530

    Python Loops

--- NishaK/PythonProgramming/Python_Loops/Python_Loop_Basics.py
+++ NishaK/PythonProgramming/Python_Loops/Python_Loop_Basics.py
@@ -349,6 +349,17 @@ for i in range(1, 6):
 print()
 print("-"*50)
 
+"""
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
+P Q R S 
+T U V 
+W X 
+Y
+"""
 num = 65
 for i in range(1, 6):
     for j in range(1, i+1):
