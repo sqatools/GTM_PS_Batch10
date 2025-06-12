87823da25b085fc17dffa34ea5226206b4969922
ArchanaMani06 <archanask206@gmail.com>
2025-06-12 12:32:52 +0900

    added list programs

--- Archana/Python_Programming/Python_String/string_practice.py
+++ Archana/Python_Programming/Python_String/string_practice.py
@@ -2,23 +2,40 @@
 # of a specified string (length must be at least 2).
 # str1 = input("Enter the string:")
 str1 = "Excellent"
-output1 = (str1[0]+str1[-1]) * 4
+output1 = (str1[0] + str1[-1]) * 4
 print(output1)
 
-#2. find the most simultaneously repeated character in the input string.
-str2 = "He is a Excellent Volley ball player"
+# 2. find the most simultaneously repeated character in the input string.
+str2 = "He is a Excelllent Volley balll player "
 max_count = 1
 max_repeat_char = ""
 temp = 0
-print(len(str2))
-print(range(len(str2)-1))
-for i in range(len(str2)-1):
-    if str2[i] == str2[i+1]:
-        temp = temp + 1
+for i in range(len(str2) - 1):
+    if str2[i] == str2[i + 1]:
+        temp += 1
         if temp > max_count:
             max_count = temp
             max_repeat_char = str2[i]
     else:
         temp = 1
+print("Max Count:", max_count)
+print("Max Char:", max_repeat_char)
+
+
+
+
+# 3. find the most occurance character in the input string.
+str2 = "He is a Excelllent Volley balll player "
+char_count = {}
+max_count = 0
+max_repeat_char = ''
+
+for char in str2:
+    if char != ' ':  # skip spaces (optional)
+        char_count[char] = char_count.get(char, 0) + 1 #char_count.get(char, 0) returns the current count for char, or 0 if it doesn’t exist yet.
+        if char_count[char] > max_count:
+            max_count = char_count[char]
+            max_repeat_char = char
+
 print("Max Count:", max_count)
 print("Max Char:", max_repeat_char)
