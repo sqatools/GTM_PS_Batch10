61d8832e0d818230ae50d1c4defecf86d5fa4c0c
sruthi273030 <sruthikd91@gmail.com>
2025-06-16 20:21:39 +0530

    Assignment updates

---
+++ sruthi/Tuple HW.py
@@ -0,0 +1,8 @@
+#get common values from bot tuples
+tup6=(4,7,9,23,5,6,12)
+tup7=(12,4,7,9,34,6,45)
+commonvalue= ()
+for commonvalue in tup7:
+    if commonvalue in tup6:
+         print(commonvalue)
+
