608683d1d5bcc185ed9e0733b5cd2ef58da5ded0
Bi12Python <xovek11875@bitvoo.com>
2025-06-17 19:30:58 +0530

    dict program session1

--- Deepesh/PythonProgramming/Python_Dictionary/Python_dictionary.py
+++ Deepesh/PythonProgramming/Python_Dictionary/Python_dictionary.py
@@ -50,7 +50,7 @@ Key : d value : 400
 
 print("_"*50)
 ###################################
-# write a python program to get the square of even value and cube of odd values in dict
+#Q1 write a python program to get the square of even value and cube of odd values in dict
 dict2 = {'p': 5, 'q': 8, 'r': 11, 's': 12, 't': 7}
 #output = {'p': 125, 'q': 64, 'r': 1331, 's': 144, 't': 343}
 output = {}
@@ -64,3 +64,62 @@ for key, value in dict2.items():
 
 print("output :", output)
 # output : {'p': 125, 'q': 64, 'r': 1331, 's': 144, 't': 343}
+
+
+
+print("_"*50)
+###################################
+# write a python to get count of each character in the given string without using count method.
+str1 = "Python Programming"
+#output = {'P': 2, 'y':1, 't':1, 'h':1, 'o': 2, 'n': 2, ' ': 1, 'r':2, 'g': 2, 'a': 1, 'm': 2, 'i':1}
+output = {}
+
+for char in str1: # P, y, t, h, o, n, P
+    if char not in output: #
+        output[char] = 1
+        # {'P': 1, 'y': 1, 't': 1, 'h': '1', 'o': 1, 'n': 1, ' ': 1, 'r': 1}
+    else:
+        output[char] += 1
+        # {'P': 2, 'y': 1, 't': 1, 'h': '1', 'o': 1, 'n': 1, ' ': 1, 'r': 1}
+
+print("output :", output)
+# {'P': 2, 'y': 1, 't': 1, 'h': 1, 'o': 2, 'n': 2, ' ': 1, 'r': 2, 'g': 2, 'a': 1, 'm': 2, 'i': 1}
+
+
+# dict methods
+########################################################
+print(dir(dict))
+# 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop',
+# 'popitem', 'setdefault', 'update', 'values'
+
+print("_"*50)
+######################
+# get method: This method return the value of specific key.
+user1 = {'name': 'Mehul', 'password': "$$#$#$$", 'email': 'test_email@gmail.com'}
+
+print(user1.get('email'))  # test_email@gmail.com
+print(user1['email']) # test_email@gmail.com
+
+
+print("_"*50)
+######################
+# update method : This method update the value one dict data to another dict.
+dict1 = {'name': 'Mehul', 'password': "$$#$#$$", 'email': 'test_email@gmail.com'}
+dict2 = {'a': 123, 'b': 456}
+dict2.update(dict1)
+print(dict2)
+
+
+print("_"*50)
+######################
+# pop() method :  This method pop value of dictionary remove data from dict using key and return it.
+
+dict3 = {'name': 'Mehul', 'password': "$$#$#$$", 'email': 'test_email@gmail.com'}
+output = dict3.pop('email') # test_email@gmail.com
+print(output) # test_email@gmail.com
+print(dict3) # {'name': 'Mehul', 'password': '$$#$#$$'}
+
+# write a program to move data from one dict to another.
+dict4 = {'A': 567, 'B': 789, 'C': 456}
+dict5 = {}
+
