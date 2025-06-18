ed822891b48a9e1089380319dcec71e3233316c5
ArchanaMani06 <archanask206@gmail.com>
2025-06-18 09:23:56 +0900

    added the dictionary type programs

--- Archana/Python_Programming/Python_Tuple/Python_Tuple_Basics.py
+++ Archana/Python_Programming/Python_Tuple/Python_Tuple_Basics.py
@@ -1,4 +1,4 @@
-# Write a tuple program to get factorial of numbers
+# 1. Write a tuple program to get factorial of numbers
 tuple4 = (2, 3, 4, 5, 6)
 # output3 = (2*1, 3*2*1, 4*3*2*1, 5*4*3*2*1, 6*5*4*3*2*1)
 output3 = []
@@ -10,7 +10,7 @@ for val in tuple4:
     output3.append(fact)
 print(tuple(output3))
 
-# Write a tuple program to get the common values from the tuples given
+# 2. Write a tuple program to get the common values from the tuples given
 tup6 = (4, 7, 9, 23, 5, 6)
 tup7 = (12, 4, 7, 9, 34, 6, 45)
 output4 = []
@@ -22,14 +22,27 @@ for val in tup7:
         continue
 print(tuple(output4))
 
-# Write a tuple program to print the email from the list of tuples values given
+# 3. Write a tuple program to print the email from the list of tuples values given
 tup5 = ('user1@gmail.com',
         984078659898,
         'user2@gmail.coim',
         984078659898,
         'user3@gmail.cm')
-output5 =[]
+output5 = []
 for val in tup5:
     if isinstance(val, str) and "@" in val:
         output5.append(val)
 print(output5)
+
+# 4. Python tuple program to create a tuple with 2 lists of data.
+list1 = [4, 6, 8]
+list2 = [7, 1, 4]
+# Output= ((4, 7), (6, 1), (8, 4))
+output6 = []
+output6 = tuple(zip(list1,list2))
+print(output6)
+
+
+
+
+
