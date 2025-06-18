fe280866ea1a611c8bc06ec094440c603dae97b7
Deepesh <xovek11875@bitvoo.com>
2025-06-18 20:09:00 +0530

    dict program session3

--- Deepesh/PythonProgramming/Python_Dictionary/Python_dictionary.py
+++ Deepesh/PythonProgramming/Python_Dictionary/Python_dictionary.py
@@ -203,4 +203,93 @@ print("output :", output2) # output : 500
 print("dict11 :", dict11)
 # {'A': 567, 'B': 789, 'C': 456, 'E': 443, 'F': 500}
 
+print("_"*50)
+#################################################################
+# shallow copy : in case of shallow if we modify any of the dictionary, then changes will reflect on another dict as
+# well.
 
+dict1 = {'a': 123, 'b' : 345, 'c': 789}
+dict2 = dict1
+dict2['d'] = 500
+dict1['e'] = 989
+
+print('dict1 :', dict1) # {'a': 123, 'b': 345, 'c': 789, 'd': 500, 'e': 989}
+print('dict2 :', dict2) #{'a': 123, 'b': 345, 'c': 789, 'd': 500, 'e': 989}
+
+
+print("_"*50)
+# Deep Copy
+
+dict_x = {'p': 777, 'q': 888, 'r': 567}
+dicty = dict_x.copy()
+dicty['s'] = 900
+print("dictx :", dict_x) # dictx : {'p': 777, 'q': 888, 'r': 567}
+print("dicty :", dicty) # {'p': 777, 'q': 888, 'r': 567, 's': 900}
+
+
+print("_"*50)
+################################ dict comprehension #################################
+
+# Get square of values in the dictionary
+dict_A = {'p': 10, 'q': 20, 'r': 30}
+
+result = {k: v**2 for k, v in dict_A.items()}
+print("result :", result)
+# {'p': 100, 'q': 400, 'r': 900}
+
+
+print("_"*50)
+# write program to get the dict where the values are even
+dict_B = {'p': 10, 'q': 15, 'r': 8, 's': 13, 't': 20}
+result2 = {k: v for k, v in dict_B.items() if v%2 == 0}
+print("result2 :", result2)
+# result2 : {'p': 10, 'r': 8, 't': 20}
+
+
+
+print("_"*50)
+# write program repeat the keys 2 time if value is even and repeat key three times if values are odd
+dict_B = {'p': 10, 'q': 15, 'r': 8, 's': 13, 't': 20}
+# output = {'pp': 10, 'qqq': 15, 'rr': 8, 'sss': 13, 'tt': 20}
+
+#result3 = {k:v*2 if v%2 == 0 else (k:v*2) for k, v in dict_B.items() }
+#print(result3)
+
+print("_"*50)
+#############################################################
+# write a python program to calculate the bill amount of fruit purchased
+
+fruits_with_price = {'Apple': 20, 'Mango': 60, 'Banana': 30, 'Lichi': 50}
+purchased_fruits = {'Apple': 5, 'Mango': 6, 'Banana': 12, 'Lichi': 10}
+
+total_bill = 0
+
+for fruit, price in fruits_with_price.items():
+    pur_fruits = purchased_fruits[fruit]
+    fruit_bill = pur_fruits*price
+    print(fruit, "|" ,price,"|", pur_fruits,"|", fruit_bill)
+    total_bill = total_bill + fruit_bill
+
+print("_"*20)
+print("Total Bill Amount :", total_bill)
+
+
+
+##########################################
+# write a python program to add duplicates values from given dictionary.
+dict_C = {'a': 10, 'b': 20, 'c': 30, 'd': 10, 'e': 20}
+#output = {'ad': 20, 'be': 40, 'c': 30}
+output = {}
+temp = ""
+# for k, v in dict_C.items():
+#     for p, q in dict_C.items():
+
+
+
+# write a python program to calculate the bill amount of fruit purchased
+
+fruit_investory = {'Apple': 100, 'Mango': 500, 'Banana': 300, 'Lichi': 250}
+fruits_with_price = {'Apple': 20, 'Mango': 60, 'Banana': 30, 'Lichi': 50}
+purchased_fruits = {'Apple': 5, 'Mango': 6, 'Banana': 12, 'Lichi': 10}
+# updated
+# fruit_investory = {'Apple': 95, 'Mango': 494, 'Banana': 288, 'Lichi': 240}
