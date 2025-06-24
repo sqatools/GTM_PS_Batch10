882a1f0230004032267840c3d081cce39df273c8
Vinod-24 <vinodneware24@gmail.com>
2025-06-24 21:59:49 +0530

    dictionary program

--- Vinod/Python_Study/ClassHW/class_hw_programe.py
+++ Vinod/Python_Study/ClassHW/class_hw_programe.py
@@ -12,20 +12,48 @@
 # print("bye")
 
 
-hieght = 18
-#int(input("what is your hieght in feet"))
-if hieght >= 3 :
-    print("yes can ride")
-    age = 13
-    if age < 12:
-        print("pay 25 rs")
-    elif age <=18:
-        print("pay 250 rs")
-    else:
-        print("rs 500")
-else:
-    print("can't ride")
-print("bye")
+# hieght = 18
+# #int(input("what is your hieght in feet"))
+# if hieght >= 3 :
+#     print("yes can ride")
+#     age = 13
+#     if age < 12:
+#         print("pay 25 rs")
+#     elif age <=18:
+#         print("pay 250 rs")
+#     else:
+#         print("rs 500")
+# else:
+#     print("can't ride")
+# print("bye")
+
+
+
+
+
+
+# list1 = [4, 6, 8, 12, 14, 20, 30, 40, 50, 60, 70]
+#
+# output = [i for i in list1 if 10 <= i <= 40]
+# print("Values between 10 and 40:", output)
+#
+# Values between 10 and 40: [12, 14, 20, 30, 40]
+
+
+
+str1 = "we are Laerning python programming"
+
+words = str1.split()
+longest_word = max(words, key=len)
+reversed_word = longest_word[::-1]
+
+print("Longest word:", longest_word)
+print("Reversed:", reversed_word)
+
+
+
+
+
 
 
 
