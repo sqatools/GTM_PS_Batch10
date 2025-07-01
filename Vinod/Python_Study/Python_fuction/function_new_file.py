823f843ca09f384ee34006472ccbdc0ba71858c1
Vinod-24 <vinodneware24@gmail.com>
2025-07-01 22:31:45 +0530

    fuction program

---
+++ Vinod/Python_Study/Python_fuction/function_new_file.py
@@ -0,0 +1,15 @@
+def addition(n1, n2):
+    print("value n1 :", n1)
+    print("value n2 :", n2)
+    print("addition value:", n1 + n2)
+
+def factorial(num):
+    fact = 1
+    for i in range(num, 0, -1):
+        fact = fact * i
+
+    print(f"factorials value {num}:", fact)
+
+if __name__ == '__main__':
+    factorial(5)
+    addition(700, 800)
