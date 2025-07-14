345c6b769c608845ac58599166793e46291175c3
Vaishnavi22111995 <vaishnavirajput111995@gmail.com>
2025-07-14 17:33:21 +0530

    Adding 1st python file

--- Vaishnavi/File_Handling_Practice_Vaish/Prgm_Filehandling_vaish/Progr_1to5_Vais.py
+++ Vaishnavi/File_Handling_Practice_Vaish/Prgm_Filehandling_vaish/Progr_1to5_Vais.py
@@ -11,3 +11,30 @@ read_mode("C:\\SeleniumTraining\\GTM_PS_Batch10\\Vaishnavi\\File_Handling_Practi
 
 #4). Python file program to get the file’s first three and last three lines.
 
+def get_three_line(filepath):
+    with open(filepath,"r") as file:
+        data=file.readlines()
+        for i in range(0,3,1):
+            print(data[i],end=" ")
+
+        for j in range(-1,-4,-1):
+            print(data[j])
+
+#get_three_line("Practice_file.txt")
+
+#5). Python file program to get all the email ids from a text file.
+def get_three_line1(filepath):
+    with open(filepath,"r") as file:
+        data=file.readlines()
+        str1=str(data)
+        list1=str1.split()
+        output=[]
+        for val in list1:
+            for char in val:
+                if char=="@":
+                    output.append(val)
+        print(output)
+
+
+get_three_line1("Practice_file.txt")
+
