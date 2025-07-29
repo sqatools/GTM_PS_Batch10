a4858e692ec2a458bdf34db30a3c8aff508eede1
knisha2511 <khandagalenisha@gmail.com>
2025-07-29 13:35:49 +0530

    Selenium browser handling

--- Deepesh/SeleniumCode/Basic_Selenium/xpath_practice.py
+++ Deepesh/SeleniumCode/Basic_Selenium/xpath_practice.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(),
 driver.maximize_window()
 driver.implicitly_wait(20)
 driver.get("https://www.redbus.in/")
