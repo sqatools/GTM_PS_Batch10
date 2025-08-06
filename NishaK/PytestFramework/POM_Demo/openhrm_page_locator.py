868eca0f5ed2127d4c3206b5ddbd2f80a28edf68
knisha2511 <khandagalenisha@gmail.com>
2025-08-06 16:38:47 +0530

    Selenium Pytest Framework POM Demo

---
+++ NishaK/PytestFramework/POM_Demo/openhrm_page_locator.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+username_field = (By.NAME, "username")
+password_field = (By.NAME, "password")
+login_button = (By.XPATH, "//button[contains(.,'Login')]")
