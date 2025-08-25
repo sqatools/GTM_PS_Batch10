9273bcb6f25c690ef858afb108b3c0445d0fa3f2
bhavnaprakash88 <bhavanaprakash888@gmail.com>
2025-08-25 13:20:12 +0530

    Gorest api

---
+++ Bhavna/PytestFrameworkCode/Automation_Framework/Test/api/test_gorest_fuctionality.py
@@ -0,0 +1,30 @@
+import pytest
+from ...Modules.API.gorest_api.gorest_api_class import GoRestAPI
+
+
+class TestGoRest:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.gr = GoRestAPI()
+
+    def test_get_all_user_info_n_verify(self, request):
+        self.gr.log.info(f"Test Name: {request.node.name}")
+        resp, st_code = self.gr.get_all_user_info()
+        assert st_code == 200
+
+    def test_create_new_user_n_verify(self, request):
+        self.gr.log.info(f"Test Name: {request.node.name}")
+        resp, st_code = self.gr.create_new_user()
+        assert st_code == 201
+        assert "id" in resp
+
+    def test_update_userinfo_n_verify(self, request):
+        self.gr.log.info(f"Test Name: {request.node.name}")
+        resp, st_code = self.gr.update_user_info()
+        assert st_code == 200
+        assert resp["status"] == "active"
+
+    def test_delete_user_and_verify(self, request):
+        self.gr.log.info(f"Test Name: {request.node.name}")
+        resp, st_code = self.gr.delete_user_and_verify()
+        assert st_code == 204
